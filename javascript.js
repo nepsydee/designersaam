18c928dd9c19be4f4afbe8550deebc65c946ba8d
Sameer GM <135213913+nepsydee@users.noreply.github.com>
2023-07-31 14:43:49 +0545

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,45 +1,43 @@
 var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents"); 
- 
-function opentab(tabname)
-{ 
-    for(tablink of tablinks){
-        tablink.classList.remove("active-links");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }  
-    event.currentTarget.classList.add("active-links");
-    document.getElementById(tabname).classList.add("active-tab");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-links");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-links");
+  document.getElementById(tabname).classList.add("active-tab");
 }
-   
-        var sidemenu= document.getElementById("sidemenu");
-        function openmenu(){
-            sidemenu.style.right = "0";
-            
-        } 
-        function closemenu(){
-            sidemenu.style.right = "-200px";
-            
-        }
 
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
 
-
-
-
-
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwz1Z9zBM4JY2dtMZfVqq4mgWr581vIrw2YVJESA59BOVwyBE46xv8qN8DxtcwRIz6TvQ/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully!"
-        setTimeout(function(){
-            msg.innerHTML=""
-        }, 5000)
-        form.reset()
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwz1Z9zBM4JY2dtMZfVqq4mgWr581vIrw2YVJESA59BOVwyBE46xv8qN8DxtcwRIz6TvQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// ----------------------sound------------------
+
+const audio = new Audio();
+audio.src = "./aca.mp3";
